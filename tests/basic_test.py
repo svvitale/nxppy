154d1d616405e5883c03a25bb447178c45b25540
Scott Vitale <svvitale@gmail.com>
2014-06-15 00:48:22 +0000

    Fix minor namespace issue.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -8,14 +8,14 @@ class nxppyTests(unittest.TestCase):
         """Test that it can be imported"""
         import nxppy
 
-    @TestDecorator.either_or('detect')
+    @either_or('detect')
     def test_detect_mifare_present(self):
         """Test that we can read the UID from a present Mifare card. Either this test or
         the "absent" test below will pass, but never both."""
         import nxppy
         self.assertIsInstance(nxppy.read_mifare(), str, "Card UID is not a string")
 
-    @TestDecorator.either_or('detect')
+    @either_or('detect')
     def test_detect_mifare_absent(self):
         """Test that no card results in a None response. Either this test or the "present"
         test above will pass, but never both."""
