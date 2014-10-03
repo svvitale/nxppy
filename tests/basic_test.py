cd944a2606159c8ea11ffe8075ce4ec186fd799c
Scott Vitale <svvitale@gmail.com>
2014-10-03 00:11:01 -0600

    Update tests to use class-based interface

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,5 +1,5 @@
 import unittest
-from either_or import either_or
+from tests.either_or import either_or
 
 class nxppyTests(unittest.TestCase):
     """Basic tests for the NXP Read Library python wrapper."""
@@ -15,7 +15,9 @@ class nxppyTests(unittest.TestCase):
         Either this test or the "absent" test below will pass, but never both.
         """
         import nxppy
-        self.assertIsInstance(nxppy.read_mifare(), str, "Card UID is not a string")
+        reader = nxppy.Mifare()
+        self.assertIsInstance(reader, nxppy.Mifare)
+        self.assertIsInstance(reader.select(), str, "Card UID is not a string")
 
     @either_or('detect')
     def test_detect_mifare_absent(self):
@@ -24,4 +26,6 @@ class nxppyTests(unittest.TestCase):
         Either this test or the "present" test above will pass, but never both.
         """
         import nxppy
-        self.assertIsNone(nxppy.read_mifare(), "Card UID is not None")
+        reader = nxppy.Mifare()
+        self.assertIsInstance(reader, nxppy.Mifare)
+        self.assertIsNone(reader.select(), "Card UID is not None")
