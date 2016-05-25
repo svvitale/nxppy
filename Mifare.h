eab09a2d4b33402f974bfc0f3e05b81b84818b79
Michael Schober <michael.schober@live.at>
2016-05-25 06:54:05 +0000

    made necessary changes because of the new libary

--- Mifare.h
+++ Mifare.h
@@ -26,13 +26,43 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+
+/**
+ * Header for hardware configuration: bus interface, reset of attached reader ID, onboard LED handling etc.
+ * */
+#include <phhwConfig.h>
+
+/**
+ * Reader Library Headers
+ */
+#include <ph_Status.h>
+
+/*BAL Headers*/
+#include <phbalReg.h>
+
+/*PAL Headers*/
+#include <phpalI14443p3a.h>
+#include <phpalI14443p4.h>
+#include <phpalI14443p3b.h>
+#include <phpalI14443p4a.h>
+#include <phpalMifare.h>
+
+/*AL Headers*/
+#include <phalMfc.h>
+
+#include <phacDiscLoop.h>
+
+/* Generic KeyStore Component of Reader Library Framework.*/
+#include <phKeyStore.h>
+
+/* Printf macro */
+
 /* Configuration Headers */
 /* Controls build behavior of components */
 #include <ph_NxpBuild.h>
 /* Status code definitions */
 #include <ph_Status.h>
 
-#include <phbalReg_RpiSpi.h>
 
 /* Reader Library Headers */
 /* Generic ISO14443-3A Component of
@@ -64,8 +94,8 @@
 #define UID_ASCII_BUFFER_SIZE ((UID_BUFFER_SIZE * 2) + 1)
 
 typedef struct {
-    phbalReg_RpiSpi_DataParams_t balReader;
-    phhalHw_Rc523_DataParams_t hal;
+    phbalReg_Stub_DataParams_t balReader;
+    phhalHw_Nfc_Ic_DataParams_t hal;
     phpalI14443p4_Sw_DataParams_t I14443p4;
     phpalMifare_Sw_DataParams_t palMifare;
     phpalI14443p3a_Sw_DataParams_t I14443p3a;
