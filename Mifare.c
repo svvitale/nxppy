f2a1e3b7daed04c09c10ed8c528970619013e8af
gwillz <gwillz@internode.on.net>
2016-08-12 17:48:33 +0000

    added errors.h for human readable error handling

--- Mifare.c
+++ Mifare.c
@@ -1,4 +1,5 @@
 #include "Mifare.h"
+#include "errors.h"
 
 #include <stdio.h>
 
@@ -55,100 +56,100 @@ static phStatus_t LoadProfile(void)
     sDiscLoop.pHalDataParams = &sHal_Nfc_Ic.sHal;
 
     /*
-     * These lines are added just to SIGSEG fault when non 14443-3 card is detected 
+     * These lines are added just to SIGSEG fault when non 14443-3 card is detected
      */
     /*
-     * Assign the GI for Type A 
+     * Assign the GI for Type A
      */
     sDiscLoop.sTypeATargetInfo.sTypeA_P2P.pGi = (uint8_t *) GI;
     sDiscLoop.sTypeATargetInfo.sTypeA_P2P.bGiLength = sizeof(GI);
     /*
-     * Assign the GI for Type F 
+     * Assign the GI for Type F
      */
     sDiscLoop.sTypeFTargetInfo.sTypeF_P2P.pGi = (uint8_t *) GI;
     sDiscLoop.sTypeFTargetInfo.sTypeF_P2P.bGiLength = sizeof(GI);
     /*
-     * Assign ATR response for Type A 
+     * Assign ATR response for Type A
      */
     sDiscLoop.sTypeATargetInfo.sTypeA_P2P.pAtrRes = aData;
     /*
-     * Assign ATR response for Type F 
+     * Assign ATR response for Type F
      */
     sDiscLoop.sTypeFTargetInfo.sTypeF_P2P.pAtrRes = aData;
     /*
-     * Assign ATS buffer for Type A 
+     * Assign ATS buffer for Type A
      */
     sDiscLoop.sTypeATargetInfo.sTypeA_I3P4.pAts = aData;
     /*
      ******************************************************************************************** */
 
     /*
-     * Passive Bailout bitmap configuration 
+     * Passive Bailout bitmap configuration
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_BAIL_OUT, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * Passive poll bitmap configuration. Poll for only Type A Tags. 
+     * Passive poll bitmap configuration. Poll for only Type A Tags.
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_PAS_POLL_TECH_CFG, PHAC_DISCLOOP_POS_BIT_MASK_A);
     CHECK_STATUS(status);
 
     /*
-     * Turn OFF Passive Listen. 
+     * Turn OFF Passive Listen.
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_PAS_LIS_TECH_CFG, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * Turn OFF active listen. 
+     * Turn OFF active listen.
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_ACT_LIS_TECH_CFG, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * Turn OFF Active Poll 
+     * Turn OFF Active Poll
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_ACT_POLL_TECH_CFG, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * Disable LPCD feature. 
+     * Disable LPCD feature.
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_ENABLE_LPCD, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * reset collision Pending 
+     * reset collision Pending
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_COLLISION_PENDING, PH_OFF);
     CHECK_STATUS(status);
 
     /*
-     * whether anti-collision is supported or not. 
+     * whether anti-collision is supported or not.
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_ANTI_COLL, PH_ON);
     CHECK_STATUS(status);
 
     /*
-     * Device limit for Type A 
+     * Device limit for Type A
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_TYPEA_DEVICE_LIMIT, PH_ON);
     CHECK_STATUS(status);
 
     /*
-     * Discovery loop Operation mode 
+     * Discovery loop Operation mode
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_OPE_MODE, RD_LIB_MODE_NFC);
 
     /*
-     * Bailout on Type A detect 
+     * Bailout on Type A detect
      */
     status = phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_BAIL_OUT, PHAC_DISCLOOP_POS_BIT_MASK_A);
     CHECK_SUCCESS(status);
 
     /*
-     * Return Status 
+     * Return Status
      */
     return status;
 }
@@ -159,13 +160,13 @@ phStatus_t NfcRdLibInit(void)
     phStatus_t status;
 
     /*
-     * Initialize the Reader BAL (Bus Abstraction Layer) component 
+     * Initialize the Reader BAL (Bus Abstraction Layer) component
      */
     status = phbalReg_Stub_Init(&sBalReader, sizeof(phbalReg_Stub_DataParams_t));
     CHECK_SUCCESS(status);
 
     /*
-     * Initialize the OSAL Events. 
+     * Initialize the OSAL Events.
      */
     status = phOsal_Event_Init();
     CHECK_STATUS(status);
@@ -174,7 +175,7 @@ phStatus_t NfcRdLibInit(void)
     Set_Interrupt();
 
     /*
-     * Set HAL type in BAL 
+     * Set HAL type in BAL
      */
 #ifdef NXPBUILD__PHHAL_HW_PN5180
     status = phbalReg_SetConfig(&sBalReader, PHBAL_REG_CONFIG_HAL_HW_TYPE, PHBAL_REG_HAL_HW_PN5180);
@@ -191,13 +192,13 @@ phStatus_t NfcRdLibInit(void)
     CHECK_STATUS(status);
 
     /*
-     * Open BAL 
+     * Open BAL
      */
     status = phbalReg_OpenPort(&sBalReader);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the Reader HAL (Hardware Abstraction Layer) component 
+     * Initialize the Reader HAL (Hardware Abstraction Layer) component
      */
     status = phhalHw_Nfc_IC_Init(&sHal_Nfc_Ic,
                                  sizeof(phhalHw_Nfc_Ic_DataParams_t),
@@ -206,60 +207,60 @@ phStatus_t NfcRdLibInit(void)
     CHECK_SUCCESS(status);
 
     /*
-     * Set the parameter to use the SPI interface 
+     * Set the parameter to use the SPI interface
      */
     sHal_Nfc_Ic.sHal.bBalConnectionType = PHHAL_HW_BAL_CONNECTION_SPI;
 
     Configure_Device(&sHal_Nfc_Ic);
 
     /*
-     * Set the generic pointer 
+     * Set the generic pointer
      */
     pHal = &sHal_Nfc_Ic.sHal;
 
     /*
      * Initializing specific objects for the communication with MIFARE (R) Classic cards. The MIFARE (R) Classic card
-     * is compliant of ISO 14443-3 and ISO 14443-4 
+     * is compliant of ISO 14443-3 and ISO 14443-4
      */
 
     /*
-     * Initialize the I14443-A PAL layer 
+     * Initialize the I14443-A PAL layer
      */
     status = phpalI14443p3a_Sw_Init(&spalI14443p3a, sizeof(phpalI14443p3a_Sw_DataParams_t), &sHal_Nfc_Ic.sHal);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the I14443-A PAL component 
+     * Initialize the I14443-A PAL component
      */
     status = phpalI14443p4a_Sw_Init(&spalI14443p4a, sizeof(phpalI14443p4a_Sw_DataParams_t), &sHal_Nfc_Ic.sHal);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the I14443-4 PAL component 
+     * Initialize the I14443-4 PAL component
      */
     status = phpalI14443p4_Sw_Init(&spalI14443p4, sizeof(phpalI14443p4_Sw_DataParams_t), &sHal_Nfc_Ic.sHal);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the I14443-B PAL component 
+     * Initialize the I14443-B PAL component
      */
     status = phpalI14443p3b_Sw_Init(&spalI14443p3b, sizeof(phpalI14443p3b_Sw_DataParams_t), &sHal_Nfc_Ic.sHal);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the MIFARE PAL component 
+     * Initialize the MIFARE PAL component
      */
     status = phpalMifare_Sw_Init(&spalMifare, sizeof(phpalMifare_Sw_DataParams_t), &sHal_Nfc_Ic.sHal, NULL);
     CHECK_STATUS(status);
 
     /*
-     * Initialize the discover component 
+     * Initialize the discover component
      */
     status = phacDiscLoop_Sw_Init(&sDiscLoop, sizeof(phacDiscLoop_Sw_DataParams_t), &sHal_Nfc_Ic.sHal);
     CHECK_SUCCESS(status);
 
     /*
-     * Load profile for Discovery loop 
+     * Load profile for Discovery loop
      */
     LoadProfile();
 
@@ -267,7 +268,7 @@ phStatus_t NfcRdLibInit(void)
     CHECK_STATUS(status);
 
     /*
-     * Read the version of the reader IC 
+     * Read the version of the reader IC
      */
 #if defined NXPBUILD__PHHAL_HW_RC523
     status = phhalHw_Rc523_ReadRegister(&sHal_Nfc_Ic.sHal, PHHAL_HW_RC523_REG_VERSION, &bDataBuffer[0]);
@@ -279,7 +280,7 @@ phStatus_t NfcRdLibInit(void)
 #endif
 
     /*
-     * Return Success 
+     * Return Success
      */
     return PH_ERR_SUCCESS;
 }
@@ -289,15 +290,13 @@ PyObject *Mifare_init(Mifare * self, PyObject * args, PyObject * kwds)
 
     int ret;
     ret = Set_Interface_Link();
-    if (ret != PH_ERR_SUCCESS) {
-        return PyErr_Format(ReadError, "Hardware Initialisaton failed: %04x", ret);
-    }
+    if (handle_error(ret, ReadError)) return NULL;
+    
     Reset_reader_device();
 
     ret = NfcRdLibInit();
-    if (ret != PH_ERR_SUCCESS) {
-        return PyErr_Format(ReadError, "Hardware Initialisaton failed: %04x", ret);
-    }
+    if (handle_error(ret, ReadError)) return NULL;
+    
     Py_RETURN_NONE;
 }
 
@@ -307,70 +306,62 @@ PyObject *Mifare_select(Mifare * self)
     uint16_t wTagsDetected = 0;
 
     /*
-     * Field OFF 
+     * Field OFF
      */
     status = phhalHw_FieldOff(pHal);
     CHECK_STATUS(status);
-    if (status != PH_ERR_SUCCESS) {
-        return PyErr_Format(SelectError, "FieldOff command failed: %4x", status);
-    }
+    if (handle_error(status, SelectError)) return NULL;
+    
     /*
-     * Configure Discovery loop for Poll Mode 
+     * Configure Discovery loop for Poll Mode
      */
-    status =
-        phacDiscLoop_SetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_NEXT_POLL_STATE, PHAC_DISCLOOP_POLL_STATE_DETECTION);
+    status = phacDiscLoop_SetConfig(&sDiscLoop,
+                                    PHAC_DISCLOOP_CONFIG_NEXT_POLL_STATE,
+                                    PHAC_DISCLOOP_POLL_STATE_DETECTION);
     CHECK_STATUS(status);
-    if (status != PH_ERR_SUCCESS) {
-        return PyErr_Format(SelectError, "DiscLoop_SetConfig command failed: %4x", status);
-    }
+    if (handle_error(status, SelectError)) return NULL;
+    
     /*
-     * Run Discovery loop 
+     * Run Discovery loop
      */
     status = phacDiscLoop_Run(&sDiscLoop, PHAC_DISCLOOP_ENTRY_POINT_POLL);
-    if (((status & PH_ERR_MASK) != PHAC_DISCLOOP_DEVICE_ACTIVATED)) {
-        if (status == (PHAC_DISCLOOP_NO_TECH_DETECTED | PH_COMP_AC_DISCLOOP)) {
-            return PyErr_Format(SelectError, "No Card detected: %4x", status);
-        } else {
-            return PyErr_Format(SelectError, "DiscLoop_Run command failed: %4x", status);
+    if ((status & PH_ERR_MASK) != PHAC_DISCLOOP_DEVICE_ACTIVATED) {
+        if (handle_error(status, SelectError)) {
+            return NULL;
+        } else { // handle_error should catch everything, but if it doesn't
+            return PyErr_Format(SelectError, "DiscLoop_Run command failed: %02X", (status & PH_ERR_MASK));
         }
     }
+    
     /*
-     * Card detected 
-     */
-    /*
-     * Get the tag types detected info 
+     * Card detected
+     * Get the tag types detected info
      */
     status = phacDiscLoop_GetConfig(&sDiscLoop, PHAC_DISCLOOP_CONFIG_TECH_DETECTED, &wTagsDetected);
+    if (handle_error(status, SelectError)) return NULL;
 
     /*
-     * Check for Status 
+     * Check for Type A tag detection
      */
-    if ((status & PH_ERR_MASK) == PH_ERR_SUCCESS) {
-        /*
-         * Check for Type A tag detection 
-         */
-        if (PHAC_DISCLOOP_CHECK_ANDMASK(wTagsDetected, PHAC_DISCLOOP_POS_BIT_MASK_A)) {
+    if (PHAC_DISCLOOP_CHECK_ANDMASK(wTagsDetected, PHAC_DISCLOOP_POS_BIT_MASK_A)) {
 
-            uint8_t byteBufferSize = sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1;
-            uint8_t i;
-            char asciiBuffer[sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1];
+        uint8_t byteBufferSize = sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1;
+        uint8_t i;
+        char asciiBuffer[sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1];
 
-            if (byteBufferSize + 1 > sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1) {
-                // Truncate if we got back too much data
-                byteBufferSize = sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize;
-            }
-
-            for (i = 0; i < byteBufferSize; i++) {
-                sprintf(&asciiBuffer[2 * i], "%02X", sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].aUid[i]);
-            }
-
-            return PyUnicode_FromString(asciiBuffer);
-
-        } else {
-            return PyErr_Format(SelectError, "DISCLOOP_CHECK_ANDMASK failed: %4x", status);
+        if (byteBufferSize + 1 > sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize + 1) {
+            // Truncate if we got back too much data
+            byteBufferSize = sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].bUidSize;
         }
+
+        for (i = 0; i < byteBufferSize; i++) {
+            sprintf(&asciiBuffer[2 * i], "%02X", sDiscLoop.sTypeATargetInfo.aTypeA_I3P3[0].aUid[i]);
+        }
+
+        return PyUnicode_FromString(asciiBuffer);
+
     } else {
-        return PyErr_Format(SelectError, "DiscLoop_GetConfig commande failed: %4x", status);
+        return PyErr_Format(SelectError, "DISCLOOP_CHECK_ANDMASK failed: %02X", (status & PH_ERR_MASK));
     }
 
     Py_RETURN_NONE;
@@ -386,9 +377,8 @@ PyObject *Mifare_read_block(Mifare * self, PyObject * args)
     phStatus_t status = 0;
 
     status = phalMful_Read(&salMfc, blockIdx, bDataBuffer);
-    if (status != PH_ERR_SUCCESS) {
-        return PyErr_Format(ReadError, "Read failed: %04x", status);
-    }
+    if (handle_error(status, ReadError)) return NULL;
+
 #if PY_MAJOR_VERSION >= 3
     return Py_BuildValue("y#", &bDataBuffer[0], MFC_BLOCK_DATA_SIZE);
 #else
@@ -406,10 +396,7 @@ PyObject *Mifare_read_sign(Mifare * self)
     phStatus_t status = 0;
 
     status = phalMful_ReadSign(&salMfc, '\0', &sign);
-    if (status != PH_ERR_SUCCESS) {
-        printf("\nStatus = %X\n", status);
-        return PyErr_Format(ReadError, "Read failed: %04x", status);
-    }
+    if (handle_error(status, ReadError)) return NULL;
 
 #if PY_MAJOR_VERSION >= 3
     return Py_BuildValue("y#", sign, bufferSize);
@@ -434,12 +421,9 @@ PyObject *Mifare_write_block(Mifare * self, PyObject * args)
     }
 
     status = phalMful_Write(&salMfc, blockIdx, data);
+    if (handle_error(status, WriteError)) return NULL;
 
-    if (status != PH_ERR_SUCCESS) {
-        return PyErr_Format(WriteError, "Write failed: %04x", status);
-    }
     Py_RETURN_NONE;
-
 }
 
 /***********************************
---
+++ errors.h
@@ -0,0 +1,199 @@
+/*
+ * Human errors descriptions
+ * matching error codes defined in ph_Status.h
+ *
+ * usage:
+ *  if (handle_error(status, ReadError)) return NULL;
+ *  >> returns error description and component
+ *
+ * OR:
+ *  if (handle_error(status, ReadError, "custom message")) return NULL;
+ *  >> returns customer error and component
+ *
+ */
+
+
+#ifndef NXPPY_ERRORS_H
+#define NXPPY_ERRORS_H
+
+#include <ph_Status.h>
+#include <phacDiscLoop.h>
+#include <Python.h>
+
+char* desc_ph_error(phStatus_t status) {
+    switch (status & PH_ERR_MASK) {
+    case PH_ERR_IO_TIMEOUT:
+        return "IO Timeout, no reply received";
+    case PH_ERR_INTEGRITY_ERROR:
+        return "Integrity Error, wrong CRC or parity detected";
+    case PH_ERR_COLLISION_ERROR:
+        return "Collision Error";
+    case PH_ERR_BUFFER_OVERFLOW:
+        return "Buffer Overflow, attempted to write beyond buffer size";
+    case PH_ERR_FRAMING_ERROR:
+        return "Framing Error, invalid frame format";
+    case PH_ERR_PROTOCOL_ERROR:
+        return "Protocol Error, response violated protocol";
+    case PH_ERR_AUTH_ERROR:
+        return "Authentication Error";
+    case PH_ERR_READ_WRITE_ERROR:
+        return "Read/Write Error, occured in RAM/ROM or Flash";
+    case PH_ERR_TEMPERATURE_ERROR:
+        return "Temperature Error, RC sensors detected overheating";
+    case PH_ERR_RF_ERROR:
+        return "RF Error";
+    case PH_ERR_INTERFACE_ERROR:
+        return "Interface Error, occured in RC communication";
+    case PH_ERR_LENGTH_ERROR:
+        return "Length Error";
+    case PH_ERR_RESOURCE_ERROR:
+        return "Resource Error";
+    case PH_ERR_TX_NAK_ERROR:
+        return "NAK Error, TX rejected sanely by the counterpart";
+    case PH_ERR_RX_NAK_ERROR:
+        return "NAK Error, RX request rejected sanely by the counterpart";
+    case PH_ERR_EXT_RF_ERROR:
+        return "Error due to External RF";
+    case PH_ERR_NOISE_ERROR:
+        return "EMVCo EMD Noise Error";
+    case PH_ERR_ABORTED:
+        return "HAL shutdown was called";
+    case PH_ERR_INTERNAL_ERROR:
+        return "Internal Error";
+    case PH_ERR_INVALID_DATA_PARAMS:
+        return "Invalid Data Parameters Error, layer ID check failed";
+    case PH_ERR_INVALID_PARAMETER:
+        return "Parameter Error, invalid parameter supplied";
+    case PH_ERR_PARAMETER_OVERFLOW:
+        return "Parameter Overflow, reading/writing produced an overflow";
+    case PH_ERR_UNSUPPORTED_PARAMETER:
+        return "Parameter not supported";
+    case PH_ERR_UNSUPPORTED_COMMAND:
+        return "Command not supported";
+    case PH_ERR_USE_CONDITION:
+        return "Condition Error";
+    case PH_ERR_KEY:
+        return "Key Error";
+    case PH_ERR_OSAL_ERROR:
+        return "OSAL Error occurred during initialization";
+    // from phacDiscloop.h
+    case PHAC_DISCLOOP_FAILURE:
+        return "Failure due to error from lower layer";
+    case PHAC_DISCLOOP_COLLISION_PENDING:
+        return "Collision pending";
+    case PHAC_DISCLOOP_EXTERNAL_RFON:
+        return "External RF field on";
+    case PHAC_DISCLOOP_EXTERNAL_RFOFF:
+        return "External RF field off";
+    case PHAC_DISCLOOP_NO_TECH_DETECTED:
+        return "No card/device detected";
+    case PHAC_DISCLOOP_NO_DEVICE_RESOLVED:
+        return "No card/device resolved";
+    case PHAC_DISCLOOP_LPCD_NO_TECH_DETECTED:
+        return "LPCD succeeded but no card/device detected";
+    case PHAC_DISCLOOP_MULTI_TECH_DETECTED:
+        return "Multiple cards/devices detected";
+    case PHAC_DISCLOOP_MULTI_DEVICES_RESOLVED:
+        return "Multiple cards/devices resolved";
+    }
+    char* buff = (char*)malloc(sizeof(char)*30);
+    sprintf(buff, "Unknown Error: %02X", (status & PH_ERR_MASK));
+    return buff;
+}
+
+
+char* desc_ph_comp(phStatus_t status) {
+    switch(status & PH_COMP_MASK) {
+    case PH_COMP_GENERIC:
+        return "Generic Component";
+    case PH_COMP_BAL:
+        return "BAL Component";
+    case PH_COMP_HAL:
+        return "HAL Component";
+    case PH_COMP_PAL_ISO14443P3A:
+        return "ISO14443-3A PAL-Component";
+    case PH_COMP_PAL_ISO14443P4A:
+        return "ISO14443-4A PAL-Component";
+    case PH_COMP_PAL_MIFARE:
+        return "Mifare PAL-Component";
+    case PH_COMP_PAL_FELICA:
+        return "FeliCa PAL-Component";
+    case PH_COMP_PAL_GENERALTARGET:
+        return "General Target/Listen mode Component";
+    case PH_COMP_AL_MFC:
+        return "Mifare Classic AL-Component";
+    case PH_COMP_AL_MFUL:
+        return "Mifare Ultralight AL-Component";
+    case PH_COMP_AL_MFP:
+        return "Mifare Plus AL-Component";
+    case PH_COMP_AL_VCA:
+        return "Virtual Card AL-Component";
+    case PH_COMP_AL_FELICA:
+        return "Open FeliCa AL-Component";
+    case PH_COMP_AL_MFDF:
+        return "Mifare DESFIRE EV1 AL-Component";
+    case PH_COMP_AL_MFDFEV2:
+        return "Mifare DESFIRE EV2 AL-Component";
+    case PH_COMP_AL_TOP:
+        return "Tag Operation AL-Component";
+        case PH_COMP_DL_AMP:
+        return "Amplifier DL-Component";
+    case PH_COMP_DL_OSCI:
+        return "Oscilloscope DL-Component";
+    case PH_COMP_DL_RDFPGA:
+        return "Reader FPGA Box DL-Component";
+    case PH_COMP_DL_MSTAMPOSC:
+        return "Master Amplifier Oscilloscope DL-Component";
+    case PH_COMP_DL_STEPPER:
+        return "Stepper DL-Component";
+    case PH_COMP_AC_DISCLOOP:
+        return "Discovery Loop Component";
+    case PH_COMP_CE_T4T:
+        return "Card Emulation T4T Component";
+    case PH_COMP_LN_LLCP:
+        return "LLCP Component";
+    case PH_COMP_NP_SNEP:
+        return "SNEP Component";
+    case PH_COMP_CIDMANAGER:
+        return "Cid Manager Component";
+    case PH_COMP_CRYPTOSYM:
+        return "CryptoSym Component";
+    case PH_COMP_KEYSTORE:
+        return "KeyStore Component";
+    case PH_COMP_TOOLS:
+        return "Tools Component";
+    case PH_COMP_CRYPTORNG:
+        return "CryptoRng Component";
+    case PH_COMP_LOG:
+        return "Log Component";
+    case PH_COMP_OSAL:
+        return "OS AL Component";
+    case PH_COMP_PLATFORM:
+        return "MicroController Platform Component";
+    }
+    
+    char* buff = (char*)malloc(sizeof(char)*35);
+    sprintf(buff, "Undefined Component: %02X", (status & PH_COMP_MASK));
+    return buff;
+}
+
+int handle_error_msg(phStatus_t status, PyObject* errorType, char* message) {
+    // No error, alls good
+    if (status == PH_ERR_SUCCESS) {
+        return false;
+    }
+    else if (message != NULL) {
+        PyErr_Format(errorType, "Nxppy: %d, %s from %s", status, message, desc_ph_comp(status));
+        return true;
+    }
+    else {
+        PyErr_Format(errorType, "Nxppy: %s from %s", desc_ph_error(status), desc_ph_comp(status));
+        return true;
+    }
+}
+
+int handle_error(phStatus_t status, PyObject* errorType) {
+    return handle_error_msg(status, errorType, NULL);
+}
+
+#endif // NXPPY_ERRORS_H
