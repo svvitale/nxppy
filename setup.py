1b6e3a1f230c85f36fe18f6e97950ceba8d21cc1
Scott Vitale <svvitale@gmail.com>
2016-12-25 04:21:51 +0000

    Cleanup setup.py a little, and add a requirements-dev.txt for anyone developing against nxppy

--- setup.py
+++ setup.py
@@ -1,35 +1,37 @@
 from setuptools import setup
 from distutils.core import Extension
 from distutils.command.build import build
-import os
 import sys
 from subprocess import call
-import multiprocessing
-from glob import glob
-
 
 nxppy = Extension('nxppy',
-                    define_macros = [('LINUX',None),('NATIVE_C_CODE',None),('NXPBUILD_CUSTOMER_HEADER_INCLUDED',None),('NXPBUILD__PHHAL_HW_RC523',None)],
-                    extra_compile_args=['-O0',
-                                        '-std=gnu99',
-                                        '-isystemnxp/nxprdlib/NxpRdLib/intfs',
-                                        '-isystemnxp/nxprdlib/NxpRdLib/types',
-                                        '-isystemnxp/nxprdlib/NxpRdLib',
-                                        '-isystemnxp/linux/intfs',
-                                        '-isystemnxp/linux/comps/phbalReg/src/Linux',
-                                        '-isystemnxp/linux/shared',
-                                        '-isystemnxp/examples/NfcrdlibEx4_MIFAREClassic/intfs',
-                                        '-isystemnxp/nxprdlib/NxpRdLib/comps/phbalReg/src/Stub',
-                                        '-isystemnxp/linux/comps/phPlatform/src/Posix',
-                                        '-isystemnxp/linux/comps/phOsal/src/Posix'
-                    ],
-                    extra_link_args=['nxp/build/linux/libNxpRdLibLinuxPN512.a','-lpthread','-lrt'],
-                    sources = ['Mifare.c', 'nxppy.c']
-                 
-)
+                  define_macros=[('LINUX', None),
+                                 ('NATIVE_C_CODE', None),
+                                 ('NXPBUILD_CUSTOMER_HEADER_INCLUDED', None),
+                                 ('NXPBUILD__PHHAL_HW_RC523', None)],
+                  extra_compile_args=['-O0',
+                                      '-std=gnu99',
+                                      '-isystemnxp/nxprdlib/NxpRdLib/intfs',
+                                      '-isystemnxp/nxprdlib/NxpRdLib/types',
+                                      '-isystemnxp/nxprdlib/NxpRdLib',
+                                      '-isystemnxp/linux/intfs',
+                                      '-isystemnxp/linux/comps/phbalReg/src/Linux',
+                                      '-isystemnxp/linux/shared',
+                                      '-isystemnxp/examples/NfcrdlibEx4_MIFAREClassic/intfs',
+                                      '-isystemnxp/nxprdlib/NxpRdLib/comps/phbalReg/src/Stub',
+                                      '-isystemnxp/linux/comps/phPlatform/src/Posix',
+                                      '-isystemnxp/linux/comps/phOsal/src/Posix'
+                                      ],
+                  extra_link_args=['nxp/build/linux/libNxpRdLibLinuxPN512.a', '-lpthread', '-lrt'],
+                  sources=['Mifare.c', 'nxppy.c']
 
-class build_nxppy(build):
+                  )
+
+
+# noinspection PyShadowingBuiltins
+class BuildNxppy(build):
     def run(self):
+        # noinspection PyUnusedLocal
         def compile(extra_preargs=None):
             if sys.version_info >= (3, 0):
                 python_lib = 'python3-dev'
@@ -45,22 +47,25 @@ class build_nxppy(build):
         # Run the rest of the build
         build.run(self)
 
-short_description = 'A python extension for interfacing with the NXP PN512 NFC Reader. Targeted specifically for Raspberry Pi and the EXPLORE-NFC module'
 
+short_description = 'A python extension for interfacing with the NXP PN512 NFC Reader. Targeted specifically for ' \
+                    'Raspberry Pi and the EXPLORE-NFC module'
+
+# noinspection PyBroadException
 try:
+    # noinspection PyPackageRequirements
     import pypandoc
+
     long_description = pypandoc.convert('README.md', 'rst')
 except:
     long_description = short_description
 
-setup (name = 'nxppy',
-       version = '1.4.3',
-       description = short_description, 
-       long_description = long_description,
-       author = 'Scott Vitale',
-       author_email = 'svvitale@gmail.com',
-       url = 'http://github.com/svvitale/nxppy',
-       test_suite = 'nose.collector',
-       setup_requires=['nose>=1.0'],
-       ext_modules = [nxppy],
-       cmdclass = {'build': build_nxppy})
+setup(name='nxppy',
+      version='1.4.3',
+      description=short_description,
+      long_description=long_description,
+      author='Scott Vitale',
+      author_email='svvitale@gmail.com',
+      url='http://github.com/svvitale/nxppy',
+      ext_modules=[nxppy],
+      cmdclass={'build': BuildNxppy})
